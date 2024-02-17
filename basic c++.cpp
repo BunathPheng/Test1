727edcfc73c870511da80da0d14150a651a6c70f
BunathPheng <bunathpheng4@gmail.com>
2024-02-16 23:49:08 -1200

    Myfrist commit

---
+++ basic c++.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	system("cls");
+	cout << "Welecome to Royal University of Phom Phenh" << endl;
+	cout << "Welecome C++ programming" << endl;
+	cout << "I love you so much" << endl;
+	cout << 10 + 20 << endl;
+	cout << 20 - 30 << endl;
+	cout << 20 * 10 << endl;
+	cout << 20 / 20 << endl;
+	cout << "20 + 10 = " << 20 + 10 << endl;
+	cout << "x=10;y=20\n" <<"x + y = " << 10 + 20 << endl;
+	cout << "2-3*4+5="<< 2-3*4+5 << endl;
+return 0;
+}
